b299661e1635e7cbfb9ba43ad79cbcf06bb2d57b
Rahmat Subekti <rahmatsubekti@live.com>
2016-11-26 10:16:07 +0700

    isogram

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,10 +5,9 @@ $(function(){
     $("body").toggleClass("exp");
   });
 });
-
-{% if site.google_analytics %}(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', '{{ site.google_analytics }}', 'auto');
-  ga('send', 'pageview');{% endif %}
+{% if site.google_analytics %}(function(s,u,b,e,k,t,i){s['GoogleAnalyticsObject']=k;s[k]=s[k]||function(){
+  (s[k].q=s[k].q||[]).push(arguments)},s[k].l=1*new Date();t=u.createElement(b),
+  i=u.getElementsByTagName(b)[0];t.async=1;t.src=e;i.parentNode.insertBefore(t,i)
+})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+ga('create', '{{ site.google_analytics }}', 'auto');
+ga('send', 'pageview');{% endif %}
